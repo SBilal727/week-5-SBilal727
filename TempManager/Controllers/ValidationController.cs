cbb1e94006dd74b868769a992d2b1510a5bdbff2
Shadeed Bilal <lildee2020@gmail.com>
2026-02-22 14:54:09 -0500

    Add project files.

---
+++ TempManager/Controllers/ValidationController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TempManager.Models;
+
+namespace TempManager.Controllers
+{
+    public class ValidationController : Controller
+    {
+        private TempManagerContext context;
+        public ValidationController(TempManagerContext ctx) => context = ctx;
+
+     
+             public JsonResult CheckDate(string date)
+        {
+            var temp = context.Temps.FirstOrDefault(t => t.Date.ToString() == date);
+            
+            if (temp == null)
+            {
+                TempData["okDate"] = true;
+                return Json(true);
+            }
+            else
+                return Json("That date already exists.");
+        }
+
+
+
+
+
+
+
+
+    }
+    }
+
